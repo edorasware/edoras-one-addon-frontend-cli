8db7fdd0e3ebd831b6fbc45cdb0d38cf0bd81929
Thomas Kaul <thomas.kaul@edorasware.com>
2017-08-03 15:32:15 +0200

    Ask for name and clone template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+'use strict';
+
+import inquirer from 'inquirer';
+import { exec } from 'shelljs';
+
 console.log('Hello world: edoras-one-addon-frontend-cli');
+
+const question = {
+  type: 'input',
+  name: 'name',
+  message: `What's the name of your widget?`,
+  default: 'star-rating'
+};
+
+inquirer.prompt([question]).then(function(answers) {
+  initialize(answers.name);
+
+  const result = exec(`git clone https://github.com/edorasware/edoras-one-addon-frontend-template.git`);
+  console.log(result);
+});
+
+function initialize(aName) {
+  console.log(`Your name is ${aName}`);
+}

9d8114a0c046d7af3f6b09ae78b9aa5d9e3b2c63
Thomas Kaul <thomas.kaul@edorasware.com>
2017-08-03 17:13:35 +0200

    Refactoring

--- src/configuration.js
+++ src/configuration.js
@@ -1,3 +1,4 @@
 export default {
+  IS_EXECUTION_SILENT: true,
   TEMPLATE_REPO_URL: 'https://github.com/edorasware/edoras-one-addon-frontend-template.git'
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import configuration from './configuration';
 import inquirer from 'inquirer';
+import { noop } from 'lodash';
 import path from 'path';
 import { exec, rm } from 'shelljs';
 
@@ -14,12 +15,27 @@ const question = {
 
 const widgetPath = 'widget';
 
-inquirer.prompt([question]).then(function(answers) {
+inquirer.prompt([question]).then((answers) => {
   initialize(answers.name);
-
-  exec(`git clone ${configuration.TEMPLATE_REPO_URL} ${widgetPath}`);
+  console.log(`Creating widget...`);
+  return asPromise({code: 0}, noop);
+}).then(() => {
+  console.log(`Cloning template repository...`);
+  return cloneRepo(configuration.TEMPLATE_REPO_URL, widgetPath);
+}).then(() => {
   rm('-rf', path.join(__dirname, '..', '..', '..', widgetPath, '.git'));
 });
+
+function asPromise(result, resolve, reject) {
+  return result.code === 0 ? resolve() : reject(result.stderr);
+}
+
+function cloneRepo(repoUrl, repoPath) {
+  return new Promise((resolve, reject) => {
+    const result = exec(`git clone ${repoUrl} ${repoPath}`, {silent: configuration.IS_EXECUTION_SILENT});
+    return asPromise(result, resolve, reject);
+  });
+}
 
 function initialize(aName) {
   console.log(`Your name is ${aName}`);
